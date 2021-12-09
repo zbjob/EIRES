c612dee2ddf87f86c78b764d4c80f892e5dbc664
Bo Zhao <zhaobo@informatik.hu-berlin.de>
2021-12-10 03:04:26 +0800

    minor

--- compile.sh
+++ compile.sh
@@ -1,14 +1,19 @@
 cd src/EIRES_bushfire
+mkdir bin
 make
 
 cd ../EIRES_cluster_monitoring
+mkdir bin
 make
 
 cd ../EIRES_cost_cache
+mkdir bin
 make
 
 cd ../EIRES_LRU_cache
+mkdir bin
 make
 
 cd ../EIRES_tune_weight
+mkdir bin
 make
