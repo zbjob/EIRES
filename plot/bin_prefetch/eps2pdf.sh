cfb05cc5f012a0cf38c225eee04eb11df33703e6
Bo Zhao <zhaobo@informatik.hu-berlin.de>
2022-02-05 22:46:02 +0800

    added eps->pdf scritps

---
+++ plot/bin_prefetch/eps2pdf.sh
@@ -0,0 +1,22 @@
+epstopdf Figure5a.eps   -o Figure5a.pdf
+epstopdf Figure5b.eps   -o Figure5b.pdf
+epstopdf Figure5c.eps   -o Figure5c.pdf
+epstopdf Figure5d.eps   -o Figure5d.pdf
+
+epstopdf Figure6a.eps   -o Figure6a.pdf
+epstopdf Figure6b.eps   -o Figure6b.pdf
+epstopdf Figure6c.eps   -o Figure6c.pdf
+epstopdf Figure6d.eps   -o Figure6d.pdf
+
+epstopdf Figure7a.eps   -o Figure7a.pdf
+epstopdf Figure7b.eps   -o Figure7b.pdf
+
+epstopdf Figure8a.eps   -o Figure8a.pdf
+epstopdf Figure8b.eps   -o Figure8b.pdf
+epstopdf Figure8c.eps   -o Figure8c.pdf
+
+epstopdf Figure9a.eps   -o Figure9a.pdf
+epstopdf Figure9b.eps   -o Figure9b.pdf
+
+epstopdf Figure10a.eps   -o Figure10a.pdf
+epstopdf Figure10b.eps   -o Figure10b.pdf
--- plot/plot_all.sh
+++ plot/plot_all.sh
@@ -2,3 +2,6 @@ gnuplot plotFig-5-6-8.gnuplot
 gnuplot plotFig-5d.gnuplot
 gnuplot plotFig-6d.gnuplot
 gnuplot plotFig-7-9-10.gnuplot
+
+cd bin_prefetch
+sh eps2pdf.sh
