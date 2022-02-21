9196002a35f52336980555c7364104dcb6826af9
Bo Zhao <zhaobo@informatik.hu-berlin.de>
2022-02-21 19:56:29 +0800

    reduce BL3_greedy experimenting time

--- run/run_synthetic.sh
+++ run/run_synthetic.sh
@@ -30,10 +30,10 @@ do
     ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P2 -n BL3_greedy_"$j"run -D 50000 -f 5 -C 1-Z 0 -L 10 -u 2000  -s -p throughput_BL3_greedy_"$j"run.csv &
     ../src/EIRES_BL3_non-greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P2 -n BL3_non_greedy_"$j"run -D 100000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_non_greedy_"$j"run.csv &
 
-    ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P5 -n BL3_greedy_P5_"$j"run -D 100000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_greedy_P5_"$j"run.csv &
+    ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P5 -n BL3_greedy_P5_"$j"run -D 10000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_greedy_P5_"$j"run.csv &
     ../src/EIRES_BL3_non-greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P5 -n BL3_non_greedy_P5_"$j"run -D 100000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_non_greedy_P5_"$j"run.csv &
 
-    ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P6 -n BL3_greedy_P6_"$j"run -D 100000 -f 5 -C 1-Z 0 -L 10 -u 2000  -s -p throughput_BL3_greedy_P6_"$j"run.csv &
+    ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P6 -n BL3_greedy_P6_"$j"run -D 50000 -f 5 -C 1-Z 0 -L 10 -u 2000  -s -p throughput_BL3_greedy_P6_"$j"run.csv &
     ../src/EIRES_BL3_non-greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P6 -n BL3_non_greedy_P6_"$j"run -D 100000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_non_greedy_P6_"$j"run.csv &
     wait
 
