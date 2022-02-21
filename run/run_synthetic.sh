ab8d09696f2baf5d0b0625b8fa8f33e11ffc64a1
Bo Zhao <zhaobo@informatik.hu-berlin.de>
2022-02-21 17:11:11 +0800

    minor

--- run/run_synthetic.sh
+++ run/run_synthetic.sh
@@ -27,7 +27,7 @@ do
     ../src/EIRES_LRU_cache/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P6 -n BL2_non_greedy_LRU_P6_"$j"run -D 100000 -f 5 -C 2000 -Z 0 -L 10 -u 2000 -s -p throughput_BL2_non_greedy_LRU_P6_"$j"run.csv &
 
     #baseline 3
-    ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P2 -n BL3_greedy_"$j"run -D 100000 -f 5 -C 1-Z 0 -L 10 -u 2000  -s -p throughput_BL3_greedy_"$j"run.csv &
+    ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P2 -n BL3_greedy_"$j"run -D 50000 -f 5 -C 1-Z 0 -L 10 -u 2000  -s -p throughput_BL3_greedy_"$j"run.csv &
     ../src/EIRES_BL3_non-greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P2 -n BL3_non_greedy_"$j"run -D 100000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_non_greedy_"$j"run.csv &
 
     ../src/EIRES_BL3_greedy/bin/cep_match -F ../data/synthetic_datasets/Stream_uniform_500K.csv -c ./synthetic.eql -q P5 -n BL3_greedy_P5_"$j"run -D 100000 -f 5 -C 1 -Z 0 -L 10 -u 2000 -s -p throughput_BL3_greedy_P5_"$j"run.csv &
