ae60ca01ad91782fad93dcd0519b0279a0d418d8
Bo Zhao <bo.zhao@imperial.ac.uk>
2022-02-23 03:25:45 +0800

    added print info

--- src/EIRES_bushfire/cep/cep_match.cpp
+++ src/EIRES_bushfire/cep/cep_match.cpp
@@ -151,7 +151,7 @@ public:
         event.attributes[4] = attr_e(RawEvent.humidity);
         event.attributes[5] = attr_e(RawEvent.temperature);
 
-        cout << "!!!!! SatelliteEvent" << event.attributes[0] << event.attributes[5] << endl;
+        cout << "!!!!! SatelliteEvent" << event.attributes[4] << event.attributes[5] << endl;
         
         uint64_t t = 86400000000;
         uint64_t ts= 300000000;
