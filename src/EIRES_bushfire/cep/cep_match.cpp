1cdbe4724e2c116634aa1328f09e001267e2875e
Bo Zhao <bo.zhao@imperial.ac.uk>
2022-02-24 00:17:07 +0800

    tweaks

--- src/EIRES_bushfire/cep/cep_match.cpp
+++ src/EIRES_bushfire/cep/cep_match.cpp
@@ -81,7 +81,7 @@ public:
 		}
 
 		m_Query = !_queryName ? m_Definition.query((size_t)0) : m_Definition.query(_queryName);
-        cout << "!!!!!!!" << string(_queryName) << m_Query <<  endl;
+        cout << "[Debug] query name "<< string(_queryName) << ", pointer address " << m_Query <<  endl;
 		if (!m_Query)
 		{
 			fprintf(stderr, "query not found");
