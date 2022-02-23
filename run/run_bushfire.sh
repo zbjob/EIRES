64c7d7a72983d9e55bbae50260eea52da71cfdc0
Bo Zhao <bo.zhao@imperial.ac.uk>
2022-02-23 17:24:03 +0800

    fixed bushfire

--- run/run_bushfire.sh
+++ run/run_bushfire.sh
@@ -1,7 +1,6 @@
 for j in `seq 1 $1`
 do
     ../src/EIRES_bushfire/bin/cep_match -f ../data/bushfire_datasets/california_satellite_weather.csv -c bf-7.7_14.16.eql -q bushfireDay -F 5 -L 10000 -Z 0 -C 1 -n day_fire_BL1_"$j"_run -A -B -b -p throughput_day_fire_BL1_"$j"_run.csv
-
     ../src/EIRES_bushfire/bin/cep_match -f ../data/bushfire_datasets/california_satellite_weather.csv -c bf-7.7_14.16.eql -q bushfireDay -F 5 -L 10000 -Z 0 -C 1000 -n day_fire_BL2_"$j"_run -A -B -b -p throughput_day_fire_BL2_"$j"_run.csv
     ../src/EIRES_bushfire/bin/cep_match -f ../data/bushfire_datasets/california_satellite_weather.csv -c bf-7.7_14.16.eql -q bushfireDay -F 5 -L 10000 -Z 100 -C 1000 -n day_fire_BL3_"$j"_run -A -B -b -p throughput_day_fire_BL3_"$j"_run.csv
     ../src/EIRES_bushfire/bin/cep_match -f ../data/bushfire_datasets/california_satellite_weather.csv -c bf-7.7_14.16.eql -q bushfireDay -F 5 -L 10000 -Z 100 -C 10000 -n day_fire_PFetch_"$j"_run -A -p throughput_day_fire_PFetch_"$j"_run.csv
